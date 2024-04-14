bb66dfcaec5154bdd589789070adfc79de204421
Yi-Hung Chang <j669404@gmail.com>
2024-04-14 16:59:06 +0200

    feat: Now hello.c can greet someone by his/her name

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!\n");
+  char name[30];
+  printf("What's your name? ");
+  scanf("%s", name);
+  printf("Hello %s!\n", name);
   return 0;
 }
